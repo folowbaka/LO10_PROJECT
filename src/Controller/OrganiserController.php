448b0f3d4b9958438e73a5da285455c243229ac3
Folow <davidgalante77@gmail.com>
2018-05-23 23:44:48 +0200

    UPDATE CSS "Organiser Table"

--- src/Controller/OrganiserController.php
+++ src/Controller/OrganiserController.php
@@ -3,6 +3,7 @@
 namespace App\Controller;
 
 use App\Form\TableJeuType;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use App\Entity\TableJeu;
@@ -12,10 +13,20 @@ class OrganiserController extends Controller
     /**
      * @Route("/organiser", name="organiser")
      */
-    public function index()
+    public function index(Request $request)
     {
         $table=new TableJeu();
         $form=$this->createForm(TableJeuType::class,$table);
+        $form->handleRequest($request);
+        if($form->isSubmitted() && $form->isValid())
+        {
+            $entityManager=$this->getDoctrine()->getManager();
+            $entityManager->persist($table);
+            $entityManager->flush();
+
+            return $this->render('organiser/index.html.twig', array('form'=>$form->createView()
+            ));
+        }
         return $this->render('organiser/index.html.twig', array('form'=>$form->createView()
         ));
     }
