d5f75be1ed658696c48b7848ec3282b703a527d3
Folow <davidgalante77@gmail.com>
2018-06-09 00:50:55 +0200

    AJOUT : Lien Table et Region

--- src/Controller/OrganiserController.php
+++ src/Controller/OrganiserController.php
@@ -2,12 +2,14 @@
 
 namespace App\Controller;
 
+use App\Entity\Departement;
 use App\Entity\Region;
 use App\Form\TableJeuType;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use App\Entity\TableJeu;
+use GuzzleHttp\Client;
 
 class OrganiserController extends Controller
 {
@@ -21,10 +23,16 @@ class OrganiserController extends Controller
         $form->handleRequest($request);
         if($form->isSubmitted() && $form->isValid())
         {
+            $ville=$table->getVille();
+            $serializer=$this->container->get('jms_serializer');
+            $client= $this->get('eight_points_guzzle.client.geo_gouv');
+            $response=$client->get("/communes?nom=".$ville."&fields=departement");
+            $body=$serializer->deserialize($response->getBody()->getContents(),"array","json");
+            $codeDepartement=$body[0]["departement"]["code"];
             $entityManager=$this->getDoctrine()->getManager();
-            $region=$this->getDoctrine()
-                ->getRepository(Region::class)->find(2);
-            $table->setRegion($region);
+            $departement=$this->getDoctrine()
+                ->getRepository(Departement::class)->find($codeDepartement);
+            $table->setRegion($departement->getRegion());
             $entityManager->persist($table);
             $entityManager->flush();
 
