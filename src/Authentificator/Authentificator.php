c88b2bdd16683b0d1ef255b764c04693f489b38b
Thomas <pixelman25@gmail.com>
2018-06-11 22:32:55 +0200

    New service: Authentificator

---
+++ src/Authentificator/Authentificator.php
@@ -0,0 +1,74 @@
+<?php
+
+namespace App\Authentificator;
+
+
+use Symfony\Component\Config\Definition\Exception\Exception;
+use Portier\Client\Client;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+
+class Authentificator
+{
+
+
+    private $portier;
+
+    private $redis;
+    /**
+     * $app->get('/', function($req, JsonResponse $res) {
+     * $res = $res
+     * ->withStatus(200)
+     * ->withHeader('Content-Type', 'text/html; charset=utf-8');
+     *
+     * $res->getBody()->write(
+     * <<<EOF
+     * <p>Enter your email address:</p>
+     * <form method="post" action="/auth">
+     * <input name="email" type="email">
+     * <button type="submit">Login</button>
+     * </form>
+     * EOF
+     * );
+     *
+     * return $res;
+     * });
+     **/
+
+    public function __construct()
+    {
+        $this->redis= new \Redis();
+        $this->redis->pconnect('127.0.0.1', 6379);
+        $this->portier = new Client(
+            new \Portier\Client\RedisStore( $this->redis),
+            'http://localhost:8000/verify'
+        );
+    }
+
+    public function auth(Request $req, Response $res) : Response
+    {
+
+        $authUrl = $this->portier->authenticate($req->get('email'));
+
+        return $res
+            ->setStatusCode(303)
+            ->headers->set('Location', $authUrl);
+    }
+
+    public function verify(Request $req, Response $res) : Response
+    {
+        try {
+            $email = $this->portier->verify($req->get('id_token'));
+
+            $res = $res
+                ->setStatusCode(200)
+                ->headers->set('Content-Type', 'text/html; charset=utf-8');
+
+            //$res->getBody()->write();
+
+            return $res;
+        }catch (\Exception $e) {
+            print_r($e->getMessage());
+        }
+    }
+}
