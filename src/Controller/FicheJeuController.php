7ecf3e8cc7896877df729d88edeb893877d27c71
Folow <davidgalante77@gmail.com>
2018-06-15 01:26:16 +0200

    AJOUT Vote: Creation d'un vote

--- src/Controller/FicheJeuController.php
+++ src/Controller/FicheJeuController.php
@@ -7,6 +7,7 @@ use App\Form\JeuType;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\Routing\Generator\UrlGeneratorInterface;
 
 class FicheJeuController extends Controller
 {
@@ -31,6 +32,14 @@ class FicheJeuController extends Controller
             $entityManager=$this->getDoctrine()->getManager();
             $entityManager->persist($jeu);
             $entityManager->flush();
+            $client   = $this->get('eight_points_guzzle.client.score_api');
+            $link = $this->generateUrl(
+                'fiche_jeu', [
+                'id'=>$jeu->getId()
+            ],
+                UrlGeneratorInterface::ABSOLUTE_URL
+            );
+            $response = $client->put("/vote/$link");
         }
         return $this->render('fiche_jeu/ajouter.html.twig',array("form"=>$form->createView()));
     }
