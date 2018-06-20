4edfbb8af0f6d592f118569e82457276306dbb68
heliacerbonne <cerbonne.helia@gmail.com>
2018-06-20 08:18:50 +0200

    Display points ont the OpenLayers map

--- public/assets/js/recherche.js
+++ public/assets/js/recherche.js
@@ -1,3 +1,4 @@
+var map;
 $(document).ready(function(){
     /*
      * Display a map
@@ -15,34 +16,54 @@ $(document).ready(function(){
 
     // Map center
     var regions = {
-        'alsace': [0, 0],
-        'aquitaine': [0, 0],
-        'auvergne': [0, 0],
-        'basse-normandie': [0, 0],
-        'bourgogne': [0, 0],
-        'bretagne': [0, 0],
-        'centre': [0, 0],
-        'champagne-ardenne': [0, 0],
-        'corse': [0, 0],
-        'franche-comte': [0, 0],
-        'haute-normandie': [0, 0],
-        'ile-de-france': [0, 0],
-        'languedoc-rousillon': [0, 0],
-        'limousin': [0, 0],
-        'lorraine': [0, 0],
-        'midi-pyrenees': [0, 0],
-        'nord-pas-de-calais': [0, 0],
-        'pays-de-la-loire': [0, 0],
-        'picardie': [0, 0],
-        'poitou-charentes': [0, 0],
-        'provence-alpes-cote-dazur': [0, 0],
-        'rhone-alpes': [0, 0]
+        'alsace': [3, 46.5],
+        'aquitaine': [3, 46.5],
+        'auvergne': [3, 46.5],
+        'basse-normandie': [3, 46.5],
+        'bourgogne': [3, 46.5],
+        'bretagne': [3, 46.5],
+        'centre': [3, 46.5],
+        'champagne-ardenne': [3, 46.5],
+        'corse': [3, 46.5],
+        'franche-comte': [3, 46.5],
+        'haute-normandie': [3, 46.5],
+        'ile-de-france': [3, 46.5],
+        'languedoc-rousillon': [3, 46.5],
+        'limousin': [3, 46.5],
+        'lorraine': [3, 46.5],
+        'midi-pyrenees': [3, 46.5],
+        'nord-pas-de-calais': [3, 46.5],
+        'pays-de-la-loire': [3, 46.5],
+        'picardie': [3, 46.5],
+        'poitou-charentes': [3, 46.5],
+        'provence-alpes-cote-dazur': [3, 46.5],
+        'rhone-alpes': [3, 46.5]
     };
     var center = regions[$("#zoneSelect").val()];
     var zoom = center ? 8 : 6;
     center = center || [3, 46.5];
 
-    var map = new ol.Map({
+    var style = new ol.style.Style({
+        fill: new ol.style.Fill({
+            color: 'rgba(' +  [102, 51, 153].join(', ') + ', 0.2)',
+        }),
+        stroke: new ol.style.Stroke({
+            color: 'rebeccapurple',
+            width: 2,
+        }),
+        image: new ol.style.Circle({
+            radius: 6,
+            fill: new ol.style.Fill({
+                color: 'rebeccapurple',
+            }),
+            stroke: new ol.style.Stroke({
+                color: 'white',
+                width: 1,
+            }),
+        }),
+    });
+
+    map = new ol.Map({
         target: 'map',
         controls: [],
         layers: [bgLayer, fgLayer],
@@ -50,11 +71,29 @@ $(document).ready(function(){
             projection: ol.proj.get('EPSG:3857'),
             // Center to Metropolitan France
             center: ol.proj.fromLonLat(center),
-            zoom: zoom,
+            zoom: 6,
             // Limit zoom levels
             minZoom: 3,
             maxZoom: 17,
         }),
+    });
+
+    var listTable = document.querySelector('#listTable');
+    listTable = JSON.parse(listTable.dataset.tablesResearched);
+    listTable.forEach(function (t) {
+        var id = t[0], name = t[1], zip = t[2], uri = t[3];
+        fetch('https://geo.api.gouv.fr/communes?fields=nom,centre&format=json&geometry=centre&codePostal=' + zip)
+        .then(function (res) { return res.json(); })
+        .then(function (commune) {
+            var city = commune[0].nom;
+            var feat = commune[0].centre;
+            var olFeat = new ol.Feature({
+                geometry: new ol.geom.Point(ol.proj.transform(feat.coordinates, 'EPSG:4326','EPSG:3857')),
+                name: name
+            });
+            olFeat.setStyle(style);
+            fgLayer.getSource().addFeature(olFeat);
+        });
     });
 
 
@@ -93,5 +132,3 @@ $(document).ready(function(){
     });
     autocompleteCity(document.getElementById("inputSearchVilleCp"));
 });
-var listTable = document.querySelector('#listTable');
-listTable = JSON.parse(listTable.dataset.tablesResearched);
